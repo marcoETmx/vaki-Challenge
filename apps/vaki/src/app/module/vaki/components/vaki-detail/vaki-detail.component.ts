f64f35b82ac204f80da9ff569d1e79cc49a256b3
Marco Elizalde <m.elizalde.torres@gmail.com>
2021-01-31 17:07:58 -0600

    add(web) se recupera la informacion desde firebase

--- apps/vaki/src/app/module/vaki/components/vaki-detail/vaki-detail.component.ts
+++ apps/vaki/src/app/module/vaki/components/vaki-detail/vaki-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Vaki } from '@vaki-challenge/models';
 
 @Component({
   selector: 'vaki-challenge-vaki-detail',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vaki-detail.component.scss']
 })
 export class VakiDetailComponent implements OnInit {
+
+  @Input() vaki: Vaki;
 
   constructor() { }
 
--- apps/vaki/src/app/module/vaki/components/vaki/vaki.component.ts
+++ apps/vaki/src/app/module/vaki/components/vaki/vaki.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { VakiService } from 'apps/vaki/src/app/core/services/entity/vaki.service';
 import { Observable } from 'rxjs';
+import { Vaki } from '@vaki-challenge/models';
 
 @Component({
   selector: 'vaki-challenge-vaki',
@@ -9,12 +10,12 @@ import { Observable } from 'rxjs';
 })
 export class VakiComponent implements OnInit {
 
-  vaki$: Observable<>
+  vaki$: Observable<Vaki>
 
   constructor(private vakiServices: VakiService) {}
 
   ngOnInit(): void {
-    vaki$ = this.vakiServices.getVaki()
+    this.vaki$ = this.vakiServices.getVaki('sa')
   }
 
   scroll(el: HTMLElement) {
